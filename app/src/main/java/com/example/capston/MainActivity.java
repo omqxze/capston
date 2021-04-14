b5031efaf5f891db3d5c4c5509dec454e7f54718
omqxze <joker9986@naver.com>
2021-04-14 18:58:08 +0900

    0.0.8

--- app/src/main/java/com/example/capston/MainActivity.java
+++ app/src/main/java/com/example/capston/MainActivity.java
@@ -1,31 +1,60 @@
 package com.example.capston;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
+        public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView);
+
+        Retrofit retrofit =new Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build();
+
+
+        GitHubClient client = retrofit.create(GitHubClient.class);
+
+        Call<List<GitHubRepo>> call = client.reposForUser("wonseok5893");
+
+        call.enqueue(new Callback<List<GitHubRepo>>() {
         @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
-                Button button = (Button) findViewById(R.id.nextActivity);
-                button.setOnClickListener(v -> {
-                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                        startActivity(intent);
-                });
-
-                Button button2 = (Button) findViewById(R.id.nextActivity2);
-                button2.setOnClickListener(v -> {
-                        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                        startActivity(intent);
-                });
-
+        public void onFailure(@NotNull Call<List<GitHubRepo>> call, @NonNull Throwable t) {
+        Log.e("debugTest","error:(${t.message})");
         }
 
+        @Override
+        public void onResponse(Call<List<GitHubRepo>>call, Response<List<GitHubRepo>> response) {
+            if(response.isSuccessful() && response.body()!=null){
+                List<GitHubRepo> repos = response.body();
+                String reposStr = "";
+                if (repos!=null){
+                    for (GitHubRepo it:repos
+                         ) {
+                        reposStr += it.name + '\n';
+                    }
+                }
+                textView.setText(reposStr);
+            }
 
-}
+        }
+        });
+        }
+        }
