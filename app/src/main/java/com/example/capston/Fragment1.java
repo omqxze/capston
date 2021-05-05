52b3d00a871ffd5a0088ec8a420aef66e17d1f40
히주 <hit_u_@hijuui-MacBookAir.local>
2021-05-05 18:05:54 +0900

    5일

--- app/src/main/java/com/example/capston/Fragment1.java
+++ app/src/main/java/com/example/capston/Fragment1.java
@@ -1,4 +1,16 @@
 package com.example.capston;
 
-public class Fragment1 {
+public class Fragment1 extends Fragment {
+
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment1, container, false);
+
+
+
+        return view;
+    }
 }
