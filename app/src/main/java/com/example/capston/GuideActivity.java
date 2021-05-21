2d05c9881fc5aa4603611be3ddf0cb0ed65e2762
omqxze <joker9986@naver.com>
2021-05-21 14:13:28 +0900

    0.7.1

--- app/src/main/java/com/example/capston/GuideActivity.java
+++ app/src/main/java/com/example/capston/GuideActivity.java
@@ -1,4 +1,60 @@
 package com.example.capston;
 
-public class GuideActivity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.capston.databinding.ActivityGuideBinding;
+import com.example.capston.databinding.ActivityInfoBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class GuideActivity extends AppCompatActivity {
+    ActivityGuideBinding binding;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_guide);
+
+        // Toolbar 활성화
+        setSupportActionBar(binding.toolbar11);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        BottomNavigationView bottomNavigationView = binding.bottomNavi;
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.show:
+                        Intent intent = new Intent(GuideActivity.this, BoardActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.write:
+                        Intent intent2 = new Intent(GuideActivity.this, QRCodeActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.home:
+                        Intent intent3 = new Intent(GuideActivity.this, MainActivity.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.chat:
+                        Intent intent4 = new Intent(GuideActivity.this, ChatBoardActivity.class);
+                        startActivity(intent4);
+                        break;
+                    case R.id.reserv:
+                        Intent intent5 = new Intent(GuideActivity.this, ReservActivity.class);
+                        startActivity(intent5);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/capston/ReservActivity.java
+++ app/src/main/java/com/example/capston/ReservActivity.java
@@ -1,4 +1,60 @@
 package com.example.capston;
 
-public class ReservActivity {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.capston.databinding.ActivityGuideBinding;
+import com.example.capston.databinding.ActivityReservBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class ReservActivity extends AppCompatActivity {
+    ActivityReservBinding binding;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_guide);
+
+        // Toolbar 활성화
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        BottomNavigationView bottomNavigationView = binding.bottomNavi;
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.show:
+                        Intent intent = new Intent(ReservActivity.this, BoardActivity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.write:
+                        Intent intent2 = new Intent(ReservActivity.this, QRCodeActivity.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.home:
+                        Intent intent3 = new Intent(ReservActivity.this, MainActivity.class);
+                        startActivity(intent3);
+                        break;
+                    case R.id.chat:
+                        Intent intent4 = new Intent(ReservActivity.this, ChatBoardActivity.class);
+                        startActivity(intent4);
+                        break;
+                    case R.id.reserv:
+                        Intent intent5 = new Intent(ReservActivity.this, ReservActivity.class);
+                        startActivity(intent5);
+                        break;
+                }
+                return false;
+            }
+        });
+    }
 }
